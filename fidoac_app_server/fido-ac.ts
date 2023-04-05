751223e9216d1ecf11a39eacbc5f99a2266e8f0e
Yeoh Wei Zhu <yeohweizhu@gmail.com>
2023-04-05 14:38:29 +0200

    updated

--- fidoac_app_server/fido-ac.ts
+++ fidoac_app_server/fido-ac.ts
@@ -28,7 +28,7 @@ export class FidoAc {
     verifyZKP(fidoAcData: any): Promise<boolean> {
         console.log(`FIDOAC ZKP verify request data ${fidoAcData}`)
         console.log(`Stringified ${JSON.stringify(fidoAcData)}`)
-        var url = "http://localhost" + this.verifyEndpoint + "/?data="+Buffer.from(JSON.stringify(fidoAcData)).toString('base64')
+        var url = "http://localhost" + this.verifyEndpoint + "?data="+Buffer.from(JSON.stringify(fidoAcData)).toString('base64')
         console.log(`Request url ${url}`)
         return fetch(url, {
             method: "POST",
